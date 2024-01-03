09a1223d86ff5e9555271da065c7391e1664cb99
Liko Indra Hartawan <likoindrahartawan@indraliko.local>
2024-01-03 14:22:40 +0700

    push

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,21 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  const MONGODB_URI = process.env.MONGODB_URI;
-
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      dbName: "evently",
+  // try {
+  //   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+  //     dbName: 'evently',
+  //     bufferCommands: false,
+  //   })
+
+  //   console.log(MONGODB_URI,'connected')
+  // } catch (error) {
+  //   console.log(error)
+  // }
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+      dbName: 'evently',
       bufferCommands: false,
-    });
+  })
 
   cached.conn = await cached.promise;
+
   return cached.conn;
-};
+}
+
+
+
--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,21 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: [
-    '/',
-    'events/:id',
-    'api/webhook/clerk',
-    'api/webhook/stripe',
-    'api/uploadthing'
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
   ],
   ignoredRoutes: [
-    'events/:id',
-    'api/webhook/clerk',
-    'api/webhook/stripe',
-    'api/uploadthing'
-  ]
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
  
